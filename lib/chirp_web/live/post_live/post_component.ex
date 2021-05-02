7ac71a153cc2f4881a57c3822fec02cc59738611
clailsonlg <clailsonlucas@gmail.com>
2021-05-02 14:06:38 -0300

    Social timeline

---
+++ lib/chirp_web/live/post_live/post_component.ex
@@ -0,0 +1,57 @@
+defmodule ChirpWeb.PostLive.PostComponent do
+  use ChirpWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+    <div id="post-<%= @post.id %>" class="post">
+      <div class="row">
+        <div class="column column-10">
+          <div class="post-avatar">
+            <img src="https://avatarfiles.alphacoders.com/103/103373.png" height="32" alt="<%= @post.username %>">
+          </div>
+        </div>
+        <div class="column column-90 post-body">
+          <b>@<%= @post.username %></b>
+          <br/>
+          <%= @post.body %>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="column">
+          <a href="#" phx-click="like" phx-target="<%= @myself %>">
+            <i class="fa fa-heart"></i> <%= @post.likes_count %>
+          </a>
+        </div>
+        <div class="column">
+          <a href="#" phx-click="repost" phx-target="<%= @myself %>">
+            <i class="fa fa-retweet"></i> <%= @post.reposts_count %>
+          </a>
+        </div>
+        <div class="column">
+          <%= live_patch to: Routes.post_index_path(@socket, :edit, @post.id) do %>
+            <i class="fa fa-edit"></i>
+          <% end %>
+          <span>  </span>
+          <%= link to: "#", phx_click: "delete", phx_value_id: @post.id, data: [confirm: "Remover?"] do %>
+            <i class="fa fa-trash" onclick="carrega()"></i>
+          <% end %>
+        </div>
+      </div>
+    </div>
+    <>
+    function carrega() {
+        window.location.reload();
+    }
+    </>
+    """
+  end
+  # def handle_event("like", _, socket) do
+  #   Chirp.Timeline.inc_likes(socket.assigns.post)
+  #   {:noreply, socket}
+  # end
+  # def handle_event("repost", _, socket) do
+  #   Chirp.Timeline.inc_reposts(socket.assigns.post)
+  #   {:noreply, socket}
+  # end
+end
